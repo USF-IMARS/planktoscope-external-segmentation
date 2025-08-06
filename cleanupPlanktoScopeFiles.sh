26b6831f34485d6189e283e79bbb8a40b591b7bc
Tylar <email@tylar.info>
2025-08-06 14:37:45 -0400

    Create cleanupPlanktoScopeFiles.sh

---
+++ cleanupPlanktoScopeFiles.sh
@@ -0,0 +1,4 @@
+rm -rf ~/.local/share/planktoscope/data/clean/*
+rm -rf ~/.local/share/planktoscope/data/export/*
+rm -rf ~/.local/share/planktoscope/data/img/*
+rm -rf ~/.local/share/planktoscope/data/objects/*
