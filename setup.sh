082762b69fd39bfefd37e9ce856f5ea351b3235b
Tylar <email@tylar.info>
2025-10-17 11:00:15 -0400

    Use sudo for creating directory and extracting files
    Added sudo to mkdir and tar commands for proper permissions.

--- setup.sh
+++ setup.sh
@@ -28,9 +28,9 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plug
 # !!! MANUALLY download from
 # https://github.com/PlanktoScope/forklift/releases
 # set up install dir
-mkdir /opt/forklift
+sudo mkdir /opt/forklift
 # install
-tar -xzf forklift_0.8.0_linux_amd64.tar.gz -C /opt/forklift
+sudo tar -xzf forklift_0.8.0_linux_amd64.tar.gz -C /opt/forklift
 # set up symlink for easy usage
 sudo ln -s /opt/forklift/forklift /usr/local/bin/forklift
 # ====================================================================
