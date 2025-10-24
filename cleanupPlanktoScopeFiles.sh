c2f3d2c8a9787fd5db82d9af5b8b1997c93771da
Tylar <email@tylar.info>
2025-10-24 15:41:57 -0400

    Enhance cleanup script with warning and confirmation
    Added a warning message and confirmation prompt before deleting files.

--- cleanupPlanktoScopeFiles.sh
+++ cleanupPlanktoScopeFiles.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+echo "⚠️  This will delete all files from this local machine related to the Planktoscope."
+read -p "Press ENTER if you are sure (or Ctrl+C to cancel)..." 
 rm -rf ~/.local/share/planktoscope/data/clean/*
 rm -rf ~/.local/share/planktoscope/data/export/*
 rm -rf ~/.local/share/planktoscope/data/img/*
